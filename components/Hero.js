35757373c93813e68415d528a7e20cb4e91ae7b3
Bryan Robinson <bryanlrobinson@gmail.com>
2023-10-05 13:36:33 -0400

    Fixes issue with rich text for hero

--- components/Hero.js
+++ components/Hero.js
@@ -9,9 +9,9 @@ export default function Hero({ button, title, description, image}) {
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
         <span className="block xl:inline">{title}</span>
       </h1>
-      <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+      <div className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
         <RichText content={description} />
-      </p>
+      </div>
       {button?.url ? <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
         <div className="rounded-md shadow">
            <a
