b882661a355b18be62ede8ac47ed8fd514337f7e
lowisren <lo.etheridge@hygraph.com>
2023-10-10 14:33:50 -0400

    migrate metadata to app router, add env sample

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,13 @@
+// Desc: Hero component that corresponds to CTA Hygraph component
 import Main from "./Main"
+// RichText is a component that renders Hygraph RichText JSON
 import { RichText } from "@graphcms/rich-text-react-renderer"
+
 export default function Hero({ button, title, description, image}) {
 
   return (  
     <Main>
-    <div className="grid grid-cols-4 gap-4 mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <div className="grid grid-cols-4 gap-4 px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
     <div className="col-span-3 sm:text-center lg:text-left">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
         <span className="block xl:inline">{title}</span>
@@ -16,7 +19,7 @@ export default function Hero({ button, title, description, image}) {
         <div className="rounded-md shadow">
            <a
             href={button.url}
-            className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+            className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
           >
             {button.text}
           </a>
@@ -36,7 +39,7 @@ export default function Hero({ button, title, description, image}) {
 export function FullHero(props) {
   // Full width stripe with a large image to the right and title and description and buttons on the left
   return (<>
-    <div className="mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="px-4 mx-auto sm:px-6 lg:px-8">
       <div className="lg:grid lg:grid-cols-12 lg:gap-8">
         <div className="self-center sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -49,7 +52,7 @@ export function FullHero(props) {
         <div className="rounded-md shadow">
            <a
             href={props.button.url}
-            className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+            className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
           >
             {props.button.text}
           </a>
@@ -59,8 +62,8 @@ export function FullHero(props) {
       </div> : ''}
         </div>
         
-        {props.image && <div className="lg:col-span-6 self-stretch overflow-hidden">
-      <img className="h-full w-full object-cover object-center" src={props.image} alt="" />
+        {props.image && <div className="self-stretch overflow-hidden lg:col-span-6">
+      <img className="object-cover object-center w-full h-full" src={props.image} alt="" />
     </div>}
       </div>
     </div>
