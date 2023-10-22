d908d864a1bf29683d98b918a96a6e6bd55f47b7
lowisren <lo.etheridge@hygraph.com>
2023-10-22 10:18:53 -0400

    almost working-cookie are storing, need exit draft help

--- app/api/exit-draft/route.js
+++ app/api/exit-draft/route.js
@@ -1,8 +1,15 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+//import { cookies } from 'next/headers'
+//import { redirect } from 'next/navigation'
+
+import { draftMode } from 'next/headers'
  
-export async function GET(request) {
+/* export async function GET(request) {
 
   cookies().delete('draftMode')
   redirect('/')
+}*/
+
+export async function GET(request) {
+  draftMode().disable()
+  //redirect(`/${model}/${post.productSlug}`)
 }
